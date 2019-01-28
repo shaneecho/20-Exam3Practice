dc547fd60e597826d4f3bb1eb2382728a42d9474
yans2 <yans2@rose-hulman.edu>
2019-01-28 13:59:15 -0500

    DONE

--- src/m1_sequence_mutation.py
+++ src/m1_sequence_mutation.py
@@ -6,8 +6,8 @@ This problem provides practice at:
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Shixin Yan.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -32,13 +32,28 @@ def run_test_zero_changer():
     print('  Actual:  ', test1)
 
     # -------------------------------------------------------------------------
-    # TODO: 2. Write at least 2 additional tests for the
+    # DONE: 2. Write at least 2 additional tests for the
     #    zero_changer
     # function.  Try to choose some unexpected things like empty lists
     # or an empty tuple, or a list with no zeros, etc.
     # -------------------------------------------------------------------------
+    # Test 2:
+    test2 = ([8, 4, 0, 20],[6, 3, 2])
+    expected2 = ([8, 4, 1,20], [6, 3, 2])
+    zero_changer(test2)
+    print()
+    print('Test 2:')
+    print('  Expected:', expected2)
+    print('  Actual:  ', test2)
 
-
+    # Test 3:
+    test3 = ([77, 0, 0, 1, 5, 0], [4, 4, 4], [4, 0, 4])
+    expected3 = ([77,1, 2, 1, 5, 3], [4, 4, 4], [4, 4, 4])
+    zero_changer(test3)
+    print()
+    print('Test 3:')
+    print('  Expected:', expected3)
+    print('  Actual:  ', test3)
 def zero_changer(tuple_of_lists):
     """
     What comes in:  A TUPLE of LISTs,
@@ -64,15 +79,20 @@ def zero_changer(tuple_of_lists):
       :type tuple_of_lists: tuple of list[int]
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # -------------------------------------------------------------------------
     # -------------------------------------------------------------------------
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
     #    DIFFICULTY:      7
     #    TIME ESTIMATE:  10 minutes.
-    # -------------------------------------------------------------------------
-
+    # ---------- ---------------------------------------------------------------
+    index = 0
+    for k in range(len(tuple_of_lists)):
+        for j in range(len(tuple_of_lists[k])):
+            if tuple_of_lists[k][j] == 0:
+                index = index + 1
+                tuple_of_lists[k][j] = index
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
